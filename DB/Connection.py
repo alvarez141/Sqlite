bb5aae9b5ee14d554b60c9a5b3acaaad6355af7b
Alvarez Daniel <alvarezdanieldelgadillo@gmail.com>
2024-09-24 08:46:16 +0200

    agregar datos

--- DB/Connection.py
+++ DB/Connection.py
@@ -1,9 +1,49 @@
 import sqlite3
 
 def Connection():
-    con = sqlite3.connect('DB/db_prueba/user.db')
+    con = sqlite3.connect('DB/db_prueba/users.db')
     cursor = con.cursor()
     return con, cursor
+
+def CreateTable(connection, cursor, info = False):
+    sentence = """
+    CREATE TABLE IF NOT EXISTS users(
+    ID INTEGER PRIMARY KEY NOT  NULL,
+    USER TEXT NULL,
+    LASTNAME TEXT NOT NULL,
+    EMAIL TEXT NOT NULL,
+    CLAVE TEXT NOT NULL
+    );
+    """
+    cursor.execute(sentence)
+
+    #cursor.execute("ALTER TABLE users ADD COLUMN EJEMPLO TEXT NOT NULL DEFAULT ''")
+    #cursor.execute("ALTER TABLE users DROP COLUMN EJEMPLO")
+
+    if info:
+        cursor.execute("PRAGMA table_info(users);")
+        columns = cursor.fetchall()
+        for column in columns:
+            print(column)
+    connection.close()
+
+def InserData(connection, cursor,data):
+    # setence = """
+    # INSERT INTO users (USER, LASTNAME, EMAIL, CLAVE)
+    # VALUES ('1', 'daniel', 'daniel@sunprored.es', '1234')
+    # """
+    setence = """
+    INSERT INTO users VALUES (?, ?, ?, ?, ?)
+    """
+    #cursor.execute(setence)
+    # cursor.execute(setence, data)
+    cursor.executemany(setence, data)
+    connection.commit()
+    connection.close()
+
+def query_data(connection, cursor):
+    ...
+
 
 if __name__ == '__main__':
     Connection()
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from DB.Connection import Connection
+from DB.Connection import Connection, CreateTable, InserData
 
 con , cursor = Connection()
-print(con)
-print(cursor)
+# CreateTable(con, cursor, info=True)
+# InserData(con, cursor)
+data = [
+        (3,'gabriel','alvarez', 'delgadillo-gabriel02@gmai.com',1234),
+        (4,'fabiola','alvarez', 'degadillofabiola@gmail.com.es',1234),
+    ]
+InserData(con, cursor, data)
