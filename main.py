c1988d9515df8ad57d8578cc368eea11fbf779ae
Alvarez Daniel <alvarezdanieldelgadillo@gmail.com>
2024-09-25 12:29:23 +0200

    mas datos sql

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from DB.Connection import Connection, create_table, add_data
 
 con , cursor = Connection()
 # create_table(con, cursor)
-dates =[('1', 'Juan', ''), ('2', 'Pedro', ''), ('3', 'Maria', ''),(4, 'Jose', '')]
+dates =[('1', 'Juan', ''), ('2', 'Pedro', ''), ('3', 'Maria', ''),(4, 'Jose', ''),(5, 'Luis', '')]
 add_data(con, cursor, dates = dates)
